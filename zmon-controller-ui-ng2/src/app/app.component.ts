650b480e0e42d3217195527f13f3ff45fdf74198
Emanuel Lauria <emanuel.lauria@gmail.com>
2017-01-16 18:38:46 +0100

    update to last angular2-playground commit from hackweek by J.Mussler

--- zmon-controller-ui-ng2/src/app/app.component.ts
+++ zmon-controller-ui-ng2/src/app/app.component.ts
@@ -1,22 +1,71 @@
 import { Component, OnInit } from '@angular/core';
-import { Alert, AlertService } from './services/alert.service';
+import { Alert, AlertDefinition, Entity, AlertService } from './services/alert.service';
 
 @Component({
   selector: 'zmon-app',
-  templateUrl: 'alerts.overview.html',
+  templateUrl: 'alerts.overview.html'
 })
 export class AppComponent implements OnInit {
   alerts: Alert[];
+  tags: Map<string, number>;
+  tagKeys: string[];
+  activeTag: string;
+  selectedAlert: Alert;
 
   constructor(private alertService: AlertService) {
-    this.alertService = alertService;
+    console.log('Creating AppComponent...');
+    this.activeTag = null;
+    this.tags = new Map();
   }
 
-  getAlerts(): void {
-    this.alertService.getAlerts().subscribe(alerts => this.alerts = alerts);
+  isEnabled(t: string): boolean {
+    return t === this.activeTag;
+  }
+
+  toggleTag(tag: string): void {
+    if (this.activeTag === tag) {
+      this.activeTag = null;
+    } else {
+      this.activeTag = tag;
+    }
+    this.filterAlerts();
+  }
+
+  filterAlerts(): void {
+    if (this.activeTag != null) {
+      this.alerts = this.alerts.filter(x => x.alert_definition.tags && x.alert_definition.tags.indexOf(this.activeTag) >= 0);
+    } else {
+      this.alerts = this.alerts;
+    }
+  }
+
+  onSelect(alert: Alert): void {
+    this.selectedAlert = alert;
+  }
+
+  updateAlerts(alerts: Alert[]): void {
+    let tags: Map<string, number> = new Map();
+    for (let a of alerts) {
+      if (a.alert_definition.tags && a.alert_definition.tags.length > 0) {
+        for (let t of a.alert_definition.tags) {
+          if (t in tags) {
+            tags[t] = tags[t] + 1;
+          } else {
+            tags[t] = 1;
+          }
+        }
+      }
+    }
+    this.tags = tags;
+    this.tagKeys = Object.keys(this.tags);
+    if (this.activeTag != null) {
+      this.alerts = alerts.filter(x => x.alert_definition.tags && x.alert_definition.tags.indexOf(this.activeTag) >= 0);
+    } else {
+      this.alerts = alerts;
+    }
   }
 
   ngOnInit(): void {
-    this.getAlerts();
+    this.alertService.getAlerts().subscribe(alerts => this.updateAlerts(alerts));
   }
 }
--- zmon-controller-ui-ng2/src/app/services/alert.service.ts
+++ zmon-controller-ui-ng2/src/app/services/alert.service.ts
@@ -1,27 +1,59 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
-import { Observable } from 'rxjs';
+import { Observable } from 'rxjs/Rx';
 
 export class AlertDefinition {
     id: number;
     name: string;
+    tags: string[];
+    priority: number;
+}
+
+export class AlertValue {
+    start_time: number;
+    captures: Map<string, string>;
+    value: string;
+    worker: string;
+}
+
+export class Entity {
+    entity: string;
+    result: AlertValue;
 }
 
 export class Alert {
     alert_definition: AlertDefinition;
+    entities: Entity[];
+}
+
+export class DataPoint {
+    ts: number;
+    td: number;
+    value: Object;
+}
+
+export class DataResult {
+    entity: string;
+    results: Array<DataPoint>;
 }
 
 @Injectable()
 export class AlertService {
 
     constructor(private http: Http) {
-
+        console.log('Creating service...');
     }
 
     getAlerts(): Observable<Alert[]> {
         // rest/allAlerts?team=*
-        return Observable.interval(10000).switchMap(() => this.http
+        return Observable.interval(3000).switchMap(() => this.http
                .get('/rest/allAlerts?team=*')
-        .map((r: Response) => r.json().data as Alert[]));
+        .map((r: Response) => { return r.json() as Alert[]; } ));
+    }
+
+    getAlertAndCheckData(alertId: number, limit: number): Observable<DataResult[]> {
+        return Observable.interval(15000).switchMap(() => this.http
+               .get(`/rest/checkAlertResults?alert_id=${alertId}&limit=${limit}`)
+        .map((r: Response) => { return r.json() as DataResult[]; } ));
     }
 }
